5605f01e0818afb8bf4761859f93c08e86bdd92e
Kelvin Rodriguez <kelvin060@gmail.com>
2015-07-14 22:44:28 -0400

    Add checkboxes to TodoLists

--- frontend/js/components/todo_lists/TodoList.jsx
+++ frontend/js/components/todo_lists/TodoList.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { ListItem } from 'material-ui';
+import { Checkbox, ListItem } from 'material-ui';
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+    this._onCheck = this._onCheck.bind(this);
+    this.state = {checked: false}
+  }
+
+  getStyles() {
+    return{
+      checked: {
+        color: 'grey',
+        textDecoration: 'line-through'
+      }
+    }
+  }
+
+  _onCheck() {
+    this.setState({checked: !this.state.checked});
+  }
 
   render() {
+    let styles = this.getStyles();
+    let checked = this.state.checked ? styles.checked : {}; 
+
     return (
-      <ListItem primaryText={this.props.title} />
+      <ListItem style={checked} leftCheckbox={<Checkbox onCheck={this._onCheck}/>} primaryText={this.props.title} secondaryText={this.props.description} secondaryTextLines={2}/>
     );
   }
 };
